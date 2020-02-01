415ef004117c280fe424b27d087813482a85d09f
manishsharma203 <manish1811.sharma@gmail.com>
2020-02-01 18:24:43 +0530

    Title Updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ class Home extends React.Component{
                     <img className={`rounded ${styles.cover}`} src='/UnderCnst.png' alt='Designed by Freepik'/>
                     <div className='p-5 text-left'>
                         <div className={`pt-5 ${styles.t1}`} >Meanwhile Check this out </div>
-                        <div className='row pt-2'>
+                        <div className='row pt-2 pb-5'>
                             <div className={`col-1 text-right ${styles.t1}`}>➥</div>
                             <div className='col pt-sm-2'>
                                 <div className='btn btn-success'><a className={`text-white ${styles.btn}`} href='https://village-mart-manish.netlify.com/'>Village Mart</a></div>
